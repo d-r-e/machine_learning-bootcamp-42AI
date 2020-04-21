294b2bb0db0b7952813b20c0e43eb92f1ad1125f
David R <egoz3ntrum@gmail.com>
2020-04-21 11:51:21 +0200

    dot product by d-r-e

---
+++ day00/ex04/dot.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+import numpy as np
+
+
+def dot(x, y):
+    if type(x) != np.ndarray or type(x) != type(y) or \
+            len(x) < 1 or len(y) != len(x):
+        return None
+    ret = 0
+    for i in range(len(x)):
+        ret += x[i] * y[i]
+    return ret
+
+
+if __name__ == "__main__":
+    x = np.array(range(3))
+    y = np.array(range(3))
+    if np.dot(x, y) == dot(x, y):
+        print("OK")
+    else:
+        print("NOT OK")
+
+    X = np.array([0, 15, -9, 7, 12, 3, -21])
+    Y = np.array([2, 14, -13, 5, 12, 4, -19])
+    if np.dot(X, Y) == dot(X, Y):
+        if np.dot(X,X) == dot(X,X):
+            print("OK")
+        else:
+            print("NOT OK")
+    else:
+        print("NOT OK")
