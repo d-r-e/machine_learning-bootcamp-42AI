d0f3bbd65eeab701e10f633a1bb9d31a2cfba99c
David R <egoz3ntrum@gmail.com>
2020-04-21 11:59:42 +0200

    d-r-e

--- day00/ex04/dot.py
+++ day00/ex04/dot.py
@@ -17,16 +17,10 @@ if __name__ == "__main__":
     x = np.array(range(3))
     y = np.array(range(3))
     if np.dot(x, y) == dot(x, y):
-        print("OK")
-    else:
-        print("NOT OK")
-
-    X = np.array([0, 15, -9, 7, 12, 3, -21])
-    Y = np.array([2, 14, -13, 5, 12, 4, -19])
-    if np.dot(X, Y) == dot(X, Y):
-        if np.dot(X, X) == dot(X, X):
-            print("OK")
-        else:
-            print("NOT OK")
-    else:
-        print("NOT OK")
+        X = np.array([0, 15, -9, 7, 12, 3, -21])
+        Y = np.array([2, 14, -13, 5, 12, 4, -19])
+        if np.dot(X, Y) == dot(X, Y):
+            if np.dot(X, X) == dot(X, X):
+                print("OK")
+                exit()
+    print("NOT OK")
