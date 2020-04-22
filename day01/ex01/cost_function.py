35aa5a458d8ee9e6cf718c9789a6a6ef5c41cfbf
David R <egoz3ntrum@gmail.com>
2020-04-22 16:05:47 +0200

    d-r-e

--- day01/ex01/cost_function.py
+++ day01/ex01/cost_function.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
 import numpy as np
 
+
 def cost_elem_(theta, X, Y):
-    if type(X) is not np.ndarray or type(Y) is not np.ndarray or type(theta) is not np.ndarray:
+    if type(X) != np.ndarray or type(Y) != np.ndarray or \
+            type(theta) != np.ndarray:
         return None
     if X.shape[1] + 1 != theta.shape[0] or X.shape[0] != Y.shape[0]:
         return None
@@ -13,10 +15,11 @@ def cost_elem_(theta, X, Y):
     for i in range(Y.size):
         ret[i] = ((X[i].dot(theta) - Y[i]) ** 2) * (1/(2 * Y.size))
     return ret
-    
+
 
 def cost_(theta, X, Y):
     return np.sum(cost_elem_(theta, X, Y))
+
 
 if __name__ == "__main__":
     X1 = np.array([[0.], [1.], [2.], [3.], [4.]])
@@ -26,11 +29,11 @@ if __name__ == "__main__":
     print(cost_elem_(theta1, X1, Y1))
     print(cost_(theta1, X1, Y1))
     print()
-    X2 = np.array([[0.2, 2., 20.], [0.4, 4., 40.], [0.6, 6., 60.], [0.8, 8.,80.]])
+    X2 = np.array([[0.2, 2., 20.], [0.4, 4., 40.], [0.6, 6., 60.], [0.8, 8., 80.]])
     theta2 = np.array([[0.05], [1.], [1.], [1.]])
     Y2 = np.array([[19.], [42.], [67.], [93.]])
     print(cost_elem_(theta2, X2, Y2))
     print(cost_(theta2, X2, Y2))
 
-#Bonus: Cost functions
+# Bonus: Cost functions
 # https://stats.stackexchange.com/questions/154879/a-list-of-cost-functions-used-in-neural-networks-alongside-applications
