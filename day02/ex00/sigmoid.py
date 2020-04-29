67b5c2fc8efcf904c0dff785a28f3d8cb076d583
David R <egoz3ntrum@gmail.com>
2020-04-29 17:14:16 +0200

    d-r-e

---
+++ day02/ex00/sigmoid.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+import numpy as np 
+
+def sigmoid_(x):
+    return 1 / (1 + np.exp(-x))
+
+if __name__ == "__main__":
+    x = -4
+    print(sigmoid_(x))
+    x = 2
+    print(sigmoid_(x))
+    x = np.array([-4, 2, 0])
+    print(sigmoid_(x))
