bc1e121d40c73c1f99ddee9deb35e291482a1af9
David R <egoz3ntrum@gmail.com>
2020-05-01 19:34:11 +0200

    d-r-e

--- day02/ex08/recall.py
+++ day02/ex08/recall.py
@@ -35,3 +35,13 @@ if __name__ == "__main__":
     y_true = np.array([1, 0, 0, 1, 0, 1, 0, 0])
     print(recall_score_(y_true, y_pred))
     print(recall_score(y_true, y_pred))
+    y_pred = np.array(['norminet', 'dog', 'norminet', 'norminet', 'dog', 'dog', 'dog', 'dog'])
+    y_true = np.array(['dog', 'dog', 'norminet', 'norminet', 'dog', 'norminet', 'dog', 'norminet'])
+    print(recall_score_(y_true, y_pred, pos_label='dog'))
+    print(recall_score(y_true, y_pred, pos_label='dog'))
+    # 0.75
+    # 0.75
+
+    # Test n.3
+    print(recall_score_(y_true, y_pred, pos_label='norminet'))
+    print(recall_score(y_true, y_pred, pos_label='norminet'))
