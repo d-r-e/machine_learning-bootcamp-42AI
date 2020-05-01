15681100e414c5f61372bde3bad0cb88f890567a
David R <egoz3ntrum@gmail.com>
2020-05-01 20:26:04 +0200

    d-r-e

---
+++ day02/ex09/f1_score.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import numpy as np
+from sklearn.metrics import f1_score
+from precision import precision_score_
+from recall import recall_score_
+
+
+def f1_score_(y_true, y_pred, pos_label=1):
+    """
+    Compute the f1 score.
+    F1 = 2 * (precision * recall) / (precision + recall)
+    Args:
+        y_true: a scalar or a numpy ndarray for the correct labels
+        y_pred: a scalar or a numpy ndarray for the predicted labels
+        pos_label: str or int, the class on which to report the precision_score (default=1)
+    Returns: 
+        The f1 score as a float.
+        None on any error.
+    Raises:
+        This function should not raise any Exception.
+    """
+    try:
+        prec = precision_score_(y_true, y_pred, pos_label)
+        recall = recall_score_(y_true, y_pred, pos_label)
+        return 2 * (prec * recall / (prec + recall))
+    except Exception:
+        return None
+
+if __name__ == "__main__":
+    y_pred = np.array([1, 1, 0, 1, 0, 0, 1, 1])
+    y_true = np.array([1, 0, 0, 1, 0, 1, 0, 0])
+    print(f1_score_(y_true, y_pred))
+    print(f1_score(y_true, y_pred))
+    y_pred = np.array(['norminet', 'dog', 'norminet', 'norminet', 'dog', 'dog', 'dog', 'dog'])
+    y_true = np.array(['dog', 'dog', 'norminet', 'norminet', 'dog', 'norminet', 'dog', 'norminet'])
+    print(f1_score_(y_true, y_pred, pos_label='dog'))
+    print(f1_score(y_true, y_pred, pos_label='dog'))
+    # 0.6666666666666665
+    # 0.6666666666666665
+
+    # Test n.3
+    print(f1_score_(y_true, y_pred, pos_label='norminet'))
+    print(f1_score(y_true, y_pred, pos_label='norminet'))
---
+++ day02/ex09/recall.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import numpy as np
+from sklearn.metrics import recall_score
+
+
+def recall_score_(y_true, y_pred, pos_label=1):
+    """
+    Compute the recall score. The recall score is the ability to detect all
+    the positive examples.
+    Args:
+        y_true: a scalar or a numpy ndarray for the correct labels
+        y_pred: a scalar or a numpy ndarray for the predicted labels
+        pos_label: str or int, the class on which to report the precision_score (default=1)
+    Returns: 
+        The recall score as a float.
+        None on any error.
+    Raises:
+        This function should not raise any Exception.
+    """
+    try:
+        tp = 0  # true positives
+        fn = 0  # false negatives
+        for i, elem in enumerate(y_true):
+            if y_pred[i] == pos_label and y_true[i] == pos_label:
+                tp += 1
+            if y_pred[i] != pos_label and y_true[i] == pos_label:
+                fn += 1
+        return (tp / (tp + fn))
+    except Exception:
+        return None
+
+
+if __name__ == "__main__":
+    y_pred = np.array([1, 1, 0, 1, 0, 0, 1, 1])
+    y_true = np.array([1, 0, 0, 1, 0, 1, 0, 0])
+    print(recall_score_(y_true, y_pred))
+    print(recall_score(y_true, y_pred))
+    y_pred = np.array(['norminet', 'dog', 'norminet', 'norminet', 'dog', 'dog', 'dog', 'dog'])
+    y_true = np.array(['dog', 'dog', 'norminet', 'norminet', 'dog', 'norminet', 'dog', 'norminet'])
+    print(recall_score_(y_true, y_pred, pos_label='dog'))
+    print(recall_score(y_true, y_pred, pos_label='dog'))
+    # 0.75
+    # 0.75
+
+    # Test n.3
+    print(recall_score_(y_true, y_pred, pos_label='norminet'))
+    print(recall_score(y_true, y_pred, pos_label='norminet'))
