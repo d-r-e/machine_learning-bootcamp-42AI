963591b37cd95b5c20f7176f1c6b786acaf31856
David R <egoz3ntrum@gmail.com>
2020-05-01 20:27:13 +0200

    d-r-e

--- day02/ex09/f1_score.py
+++ day02/ex09/f1_score.py
@@ -12,8 +12,9 @@ def f1_score_(y_true, y_pred, pos_label=1):
     Args:
         y_true: a scalar or a numpy ndarray for the correct labels
         y_pred: a scalar or a numpy ndarray for the predicted labels
-        pos_label: str or int, the class on which to report the precision_score (default=1)
-    Returns: 
+        pos_label: str or int, the class on which to report
+        the precision_score (default=1)
+    Returns:
         The f1 score as a float.
         None on any error.
     Raises:
@@ -26,13 +27,16 @@ def f1_score_(y_true, y_pred, pos_label=1):
     except Exception:
         return None
 
+
 if __name__ == "__main__":
     y_pred = np.array([1, 1, 0, 1, 0, 0, 1, 1])
     y_true = np.array([1, 0, 0, 1, 0, 1, 0, 0])
     print(f1_score_(y_true, y_pred))
     print(f1_score(y_true, y_pred))
-    y_pred = np.array(['norminet', 'dog', 'norminet', 'norminet', 'dog', 'dog', 'dog', 'dog'])
-    y_true = np.array(['dog', 'dog', 'norminet', 'norminet', 'dog', 'norminet', 'dog', 'norminet'])
+    y_pred = np.array(['norminet', 'dog', 'norminet', 'norminet',
+                       'dog', 'dog', 'dog', 'dog'])
+    y_true = np.array(['dog', 'dog', 'norminet', 'norminet',
+                       'dog', 'norminet', 'dog', 'norminet'])
     print(f1_score_(y_true, y_pred, pos_label='dog'))
     print(f1_score(y_true, y_pred, pos_label='dog'))
     # 0.6666666666666665
