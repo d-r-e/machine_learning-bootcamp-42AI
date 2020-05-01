de96f03366d270a5321a6c9da136168d5bec2a8e
David R <egoz3ntrum@gmail.com>
2020-05-01 19:33:30 +0200

    d-r-e

--- day02/ex08/recall.py
+++ day02/ex08/recall.py
@@ -4,6 +4,19 @@ from sklearn.metrics import recall_score
 
 
 def recall_score_(y_true, y_pred, pos_label=1):
+    """
+    Compute the recall score. The recall score is the ability to detect all
+    the positive examples.
+    Args:
+        y_true: a scalar or a numpy ndarray for the correct labels
+        y_pred: a scalar or a numpy ndarray for the predicted labels
+        pos_label: str or int, the class on which to report the precision_score (default=1)
+    Returns: 
+        The recall score as a float.
+        None on any error.
+    Raises:
+        This function should not raise any Exception.
+    """
     try:
         tp = 0  # true positives
         fn = 0  # false negatives
