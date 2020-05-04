0465645ee875c114b88748db363550618d1ebfac
David R <egoz3ntrum@gmail.com>
2020-05-04 19:11:06 +0200

    d-r-e

--- day03/ex04/vec_reg_linear_grad.py
+++ day03/ex04/vec_reg_linear_grad.py
@@ -4,14 +4,14 @@ import numpy as np
 
 
 def vec_reg_linear_grad(y, x, theta, lambda_):
-    m =x.shape[0]
+    m = x.shape[0]
     grad = np.zeros(x.shape[1])
     h = x.dot(theta)
-    grad[0] = (1/m) * np.sum((h - y) * x[:, 0])
-    for j in range(1, x.shape[1]):
-        grad[j] = (1/m) * (np.sum((h - y) * x[:, j]) + (lambda_*theta[j]))
+    grad = (1/m) * (x.T.dot(h - y) + (lambda_ * theta))
     return grad
 
+
+"""
 if __name__ == "__main__":
     X = np.array([
         [ -6,  -7,  -9],
@@ -31,3 +31,4 @@ if __name__ == "__main__":
     # array([-192.85714286,  886.75, -679.14285714])
 
     print(vec_reg_linear_grad(Y, X, theta, 0.0))
+"""
